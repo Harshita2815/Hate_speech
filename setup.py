6f09bfb47dc9e0d482e8c669a95ec096465f9127
Harshita2815 <harshitaprakash2815@gmail.com>
2025-02-02 13:47:39 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Harshita Prakash",
+    author_email="harshitaprakash2815@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
